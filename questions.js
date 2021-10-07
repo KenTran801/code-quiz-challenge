c32f0989fdab7246e2c48269a188a37826f3ee3c
KenTran801 <utes.fan.tran@gmail.com>
2021-10-06 22:57:56 -0600

    begin basic html setup and psuedo coding for js

---
+++ questions.js
@@ -0,0 +1,13 @@
+// Create variables
+
+
+// Create questions for the quiz (arrays)
+
+// Setup timer
+
+
+// When start button is clicked begin quiz
+
+
+
+// Functiom tp generate questions and answer choices
